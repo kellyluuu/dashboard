85fbc0cc9e38b58bbd6c92d24b50739ec35e29d8
KELLY LUU <kellyluuu@gmail.com>
2022-05-01 00:17:10 -0500

    delete extra folders

--- src/components/Visitors.js
+++ src/components/Visitors.js
@@ -4,7 +4,7 @@ export default function Visitors (){
         <div className="visitors">
             <p>Visitors</p>
             <section className="visitorsSec">
-                <img src="https://st4.depositphotos.com/15881914/22506/v/1600/depositphotos_225060510-stock-illustration-modern-modern-infographic-vector-template.jpg"></img>
+                {/* <img src="https://st4.depositphotos.com/15881914/22506/v/1600/depositphotos_225060510-stock-illustration-modern-modern-infographic-vector-template.jpg"></img> */}
             </section>
         </div>
     ) 
